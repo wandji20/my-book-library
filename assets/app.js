898513210f858af22ea4fb176fd049a7b3c031a3
wandji <wbertrand10@gmail.com>
2021-04-07 19:18:00 +0100

    Add method to get read status

--- assets/app.js
+++ assets/app.js
@@ -1,14 +1,47 @@
-const bookButton = document.querySelector('.book-btn')
-const bookForm = document.querySelector('.book-form')
+const bookButton = document.querySelector('.book-btn');
+const bookForm = document.querySelector('.book-form');
+const title = document.getElementById('title');
+const author = document.getElementById('author');
+const pages = document.getElementById('pages');
+const readNodes = document.getElementsByName('read-status');
+let read;
+function getReadStatus(radio){
+  for(let i=0; i<radio.length; i++){
+    if (radio[i].checked){
+      read = radio[i].value
+    }
+  }
+}
+getReadStatus(readNodes)
+
+let library = [];
+
+function Book(title, author, pages, read){
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+}
 
 
+function addBookToLibrary(book){
+
+}
 
 function displayForm(){
-
   if (bookForm.style.display === ''){
     bookForm.style.display = 'block';
   }else{
-    bookForm.style.display = ''
+    bookForm.style.display = '';
   }
 }
+
+
+
+
+
+
+
+
+
 bookButton.addEventListener('click', displayForm)
