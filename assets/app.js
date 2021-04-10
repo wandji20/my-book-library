4e5b5ff9077cd57f0ac4b358280fdee3806160f0
wandji <wbertrand10@gmail.com>
2021-04-10 09:10:48 +0100

    Fix remove button to to remove the row and update local storage

--- assets/app.js
+++ assets/app.js
@@ -6,16 +6,20 @@ const author = document.querySelector('#author');
 const pages = document.querySelector('#pages');
 const read_inputs = document.getElementsByName('read-status');
 
-let mylibrary = [];
 
-// function serializeMyLibrary() {
+//  function serializeMyLibrary() {
+//   let mylibrary = [];
 //   localStorage.mylibrary = JSON.stringify(mylibrary);
 // }
 
-// function deserializeMyLibrary() {
-//   const books = JSON.parse(localStorage.mylibrary);
-//   return books;
-// }
+function deserializeMyLibrary() {
+  let book = [];
+
+  if (localStorage.getItem('mylibrary')){
+    books = JSON.parse(localStorage.mylibrary); 
+  }
+  return books;
+}
 
 function Book(title, author, pages, read){
   this.title = title;
@@ -34,19 +38,21 @@ function displayForm(){
 
 
 function displayBooks(){
-  // deserialised_books = deserializeMyLibrary()
-  deserialised_books = JSON.parse(localStorage.mylibrary)
+  deserialised_books = deserializeMyLibrary()
+  /// deserialised_books = JSON.parse(localStorage.mylibrary)
   const table = document.getElementById('book-list');
-
+  
   for(let i=0; i<deserialised_books.length; i++){
+    // const read_text = deserialised_books[i].read == true ? 'Already Read' : 'To Read';
+    // const read_class = deserialised_books[i].read ? 'success' : 'danger';
     const row = document.createElement('tr');
     row.setAttribute('id', i);
     row.innerHTML = `
       <td>${deserialised_books[i].title}</td>
       <td>${deserialised_books[i].author}</td>
       <td>${deserialised_books[i].pages}</td>
-      <td>${deserialised_books[i].read}</td>
-      <td><button class='btn btn-danger'>Remove Book</button></td>
+      <td><button class='btn btn-success'>${deserialised_books[i].read}</button></td>
+      <td><button class='btn btn-danger remove'>Remove Book</button></td>
       `;
     table.appendChild(row);
   }
@@ -69,12 +75,11 @@ function createBook(){
 
   getReadvlaue();
 
-  // let mylibrary = deserializeMyLibrary();
-  mylibrary = JSON.parse(localStorage.mylibrary)
+  let mylibrary = deserializeMyLibrary();
+  // mylibrary = JSON.parse(localStorage.mylibrary)
   if(title.value === '' || author.value === '' || pages.value == '' || read == null ){
     alert('please fill all fields');
   }else{
-    console.log('allfields are filled')
     let aBook = new Book(title.value, author.value, pages.value, read);
     mylibrary.push(aBook);
     localStorage.mylibrary = JSON.stringify(mylibrary);
@@ -83,18 +88,27 @@ function createBook(){
 }
 
 function removeBook(id){
-  mylibrary = JSON.parse(localStorage.mylibrary);
+  mylibrary = deserializeMyLibrary();
   mylibrary.splice(id, 1);
   localStorage.mylibrary = JSON.stringify(mylibrary);
   
 }
+
 document.querySelector('#book-list').addEventListener('click', (e) => {
-  let t_body = e.target.parentNode.parentNode.parentNode
-  let t_row = e.target.parentNode.parentNode
-  removeBook(t_row.id);
-  t_body.removeChild(t_row);
+  console.log(e.target.className.includes('remove'))
+  if (e.target.className.includes('remove')){
+    let t_body = e.target.parentNode.parentNode.parentNode
+    let t_row = e.target.parentNode.parentNode
+    removeBook(t_row.id);
+    t_body.removeChild(t_row);
+  }
 });
-  
+
+// document.querySelector('.remove').addEventListener('click', remove)
+// function remove(){
+//   console.log(this)
+// }
+
 
 form_submit.addEventListener('click', createBook);
 form_btn.addEventListener('click', displayForm);
